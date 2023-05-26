5b3dc96f2b80cb04d15c88ea909e69763c1869dd
Gyuhyeok <ghkdrbgur13@naver.com>
2023-05-26 13:12:35 +0900

    #5 feat: Database mapping for member classes
    Member classes have been modified to allow mapping with databases.

--- src/main/java/hello/login/domain/Member.java
+++ src/main/java/hello/login/domain/Member.java
@@ -4,43 +4,62 @@ import hello.login.domain.member.GenderType;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Data
+@Entity
+@Table(name = "member")
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @Column(nullable = false, length = 12)
     @Length(min = 6, max = 12)
     private String loginId; //로그인 ID
 
+    @Column(nullable = false, length = 8)
     @Length(min = 2, max = 8)
     private String name; //사용자 이름
 
-
+    @Column(nullable = false, length = 20)
     @Length(min = 8, max = 20)
     private String password;
 
+    @Column(name = "mobileCarrier", nullable = false, length = 255)
     @NotEmpty
-    private String MobileCarrier; //통신사
+    private String mobileCarrier; //통신사
+
+    @Column(nullable = false)
     @NotNull
     private Integer phone; //전화번호
+
+    @Column(nullable = false, length = 255)
     @NotNull
     private GenderType gender; //성별
 
+    @ElementCollection
+    @CollectionTable(name = "member_hobbies", joinColumns = @JoinColumn(name = "member_id"))
+    @Column(name = "hobby")
     private List<String> hobbies; //취미
+
+    @Column(length = 255)
     private String introduce; //자기소개
+
+    @Column(nullable = false)
     private Integer money; //보유금액 회원 가입 시 100000원
 
+    @ElementCollection
+    @CollectionTable(name = "purchase_items", joinColumns = @JoinColumn(name = "member_id"))
+    @MapKeyColumn(name = "item_name")
+    @Column(name = "item_count")
     private Map<String, Integer> purchaseItems = new ConcurrentHashMap<>(); //구매한 아이템의 이름과 개수
 
-
     public Member() {}
-
 }
--- src/main/java/hello/login/repository/BoardRepository.java
+++ src/main/java/hello/login/repository/BoardRepository.java
@@ -1,13 +1,8 @@
 package hello.login.repository;
 
 import hello.login.domain.Board;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 
 public interface BoardRepository {
